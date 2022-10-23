56da47948a59444d77fcc4ec2c7778033f287237
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-23 11:17:01 +0530

    Added Total Price

---
+++ Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryUC2
+{
+    public  class Inventory
+    {
+        
+        public List<TypesofRice> typesofRice;
+        public List<TypesofPulses> typesofPulses;
+        public List<TypesofWeats> typesofWeats;
+    }
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofWeats
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
+
---
+++ fetchdata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryUC2
+{
+    public class fetchdata
+    {
+        public Inventory Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Inventory>(json);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
